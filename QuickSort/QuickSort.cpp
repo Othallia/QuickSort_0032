a2fba674ee1623a165b9f95644d42f64c523f475
Othallia <vianiputriocta6@gmail.com>
2024-03-28 17:04:43 +0700

    membuat int temporary

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -32,5 +32,11 @@ void input() {
 //swasps the element at index x with the element at index y
 void swap(int x, int y)
 {
+	int temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+	mov_count++;
+
+
 
 }
